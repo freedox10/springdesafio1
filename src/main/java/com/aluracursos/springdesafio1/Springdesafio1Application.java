43d4fc1f627474bf7c0dc948c919b6cad0f7be88
freedox10 <alfre2636@yahoo.com.ar>
2024-05-02 22:30:42 -0300

    Iniciando Desafío 1 Curso Spring Boot

---
+++ src/main/java/com/aluracursos/springdesafio1/Springdesafio1Application.java
@@ -0,0 +1,13 @@
+package com.aluracursos.springdesafio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springdesafio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springdesafio1Application.class, args);
+	}
+
+}
